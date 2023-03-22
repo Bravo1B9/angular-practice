1f878a03226ded58932fad943148760e3bbc9b0f
Bravo1B9 <brofe93@gmail.com>
2023-03-22 17:18:38 +0000

    Initial posts component setup

--- basics/src/app/app.module.ts
+++ basics/src/app/app.module.ts
@@ -6,12 +6,14 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { UsersComponent } from './users/users.component';
 import { CreateUserComponent } from './create-user/create-user.component';
 import { FormsModule } from '@angular/forms';
+import { PostsComponent } from './posts/posts.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     UsersComponent,
     CreateUserComponent,
+    PostsComponent,
   ],
   imports: [
     FormsModule,
